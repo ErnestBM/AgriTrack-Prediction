8fda36d6a5a2dd9c37e75fd25936ae16794de8fa
ernestbenedictus <m010d4ky1623@bangkit.academy>
2024-06-18 20:39:34 +0700

    apa

--- prediction/settings.py
+++ prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b9-il_vl$8pc@t^=6$t)6ajmy@-r(fvg9lj9c^o@t^00t$7ht1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
